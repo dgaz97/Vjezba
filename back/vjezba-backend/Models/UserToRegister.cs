e80e87e98520a683b8e77369c8f7307dd8421321
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-12-19 23:53:08 +0100

    Klasa u koju se, pri registraciji, spremaju podaci o korisniku koji se ne mogu automatski generirati

---
+++ back/vjezba-backend/Models/UserToRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vjezba_backend.Models
+{
+    public class UserToRegister
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public System.DateTime birthdate { get; set; }
+        public string role_roleName { get; set; }
+    }
+}
