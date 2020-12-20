9010ef67843335a3d4c54418c7f0e91f183fe9a9
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-12-20 20:55:29 +0100

    Proširen view za registraciju, uređena provjera lozinke, te uvedena provjera korisničkog imena

--- front/app/register/register-view.component.js
+++ front/app/register/register-view.component.js
@@ -1,9 +1,9 @@
 angular.module('myApp.register-view')
     .component('registerView', {
         templateUrl: 'register/register-view.template.html',
-        controller: ['$routeParams', 'Register', function RegisterController($routeParams, Register) {
-            var self=this;
-            self.brojKorisnika=5;
+        controller: ['$routeParams', 'Register', 'CheckUsername', function RegisterController($routeParams, Register, CheckUsername) {
+            var self = this;
+            self.brojKorisnika = 5;
 
             self.username;
             self.password1;
@@ -12,16 +12,19 @@ angular.module('myApp.register-view')
             self.firstName;
             self.lastName;
 
-            self.hidePassMismatch="true";
-            self.hideBadPassFormat="true";
+            self.hidePassMismatch = "true";
+            self.hideBadPassFormat = "true";
+            self.hideUsernameExists = "true";
 
-            self.regex=[RegExp('.*[A-Z]+.*'), RegExp('.*[a-z]+.*'), RegExp('.*[0-9]+.*'), RegExp('.{8,}')];
+            //self.allGood=false;
+
+            self.regex = [RegExp('.*[A-Z]+.*'), RegExp('.*[a-z]+.*'), RegExp('.*[0-9]+.*'), RegExp('.{8,}')];
 
 
 
-            self.user=Register.getUser({Id:self.brojKorisnika});
+            //self.user=CheckUsername.checkUsername({Username:self.username});
 
-            self.updateUser = function updateUser(){
+            /*self.updateUser = function updateUser(){
                 self.user=Register.getUser({Id:self.brojKorisnika});
             }
 
@@ -30,39 +33,59 @@ angular.module('myApp.register-view')
             }
             self.testfun2=function (){
                 angular.element(document.querySelector("#username-fg")).removeClass("has-error");
-            }
+            }*/
 
-            self.cond=function(){
+            self.passCheck = function () {
 
-                if (self.password1!=self.password2){
+                if (self.password1 != self.password2) {
                     angular.element(document.querySelector("#password1-fg")).addClass("has-error");
                     angular.element(document.querySelector("#password2-fg")).addClass("has-error");
-                    self.hidePassMismatch="false";
+                    self.hidePassMismatch = "false";
                     angular.element(document.querySelector("#check-passworddoesntmatch")).removeClass("ng-hide");
+                    //self.allGood=false;
                     return;
-                }else{
+                } else {
                     angular.element(document.querySelector("#password1-fg")).removeClass("has-error");
                     angular.element(document.querySelector("#password2-fg")).removeClass("has-error");
-                    self.hidePassMismatch="true";
+                    self.hidePassMismatch = "true";
                     angular.element(document.querySelector("#check-passworddoesntmatch")).addClass("ng-hide");
                 }
 
-                if (!(self.regex[0].test(self.password1)&&self.regex[1].test(self.password1)&&self.regex[2].test(self.password1)&&self.regex[3].test(self.password1))){
+                if (!(self.regex[0].test(self.password1) && self.regex[1].test(self.password1) && self.regex[2].test(self.password1) && self.regex[3].test(self.password1))) {
                     angular.element(document.querySelector("#password1-fg")).addClass("has-error");
                     angular.element(document.querySelector("#password2-fg")).addClass("has-error");
-                    self.hideBadPassFormat="false";
+                    self.hideBadPassFormat = "false";
                     angular.element(document.querySelector("#check-passwordwrongformat")).removeClass("ng-hide");
+                    //self.allGood=false;
                     return;
-                }else{
+                } else {
                     angular.element(document.querySelector("#password1-fg")).removeClass("has-error");
                     angular.element(document.querySelector("#password2-fg")).removeClass("has-error");
-                    self.hideBadPassFormat="true";
+                    self.hideBadPassFormat = "true";
                     angular.element(document.querySelector("#check-passwordwrongformat")).addClass("ng-hide");
                 }
+                //self.allGood=true;
+            }
+
+            self.usernameCheck = function () {
+                self.userData = CheckUsername.checkUsername({ Username: self.username }).$promise
+                .then(function(data){
+                    if (data.status == 'exists') {
+                        angular.element(document.querySelector("#username-fg")).addClass("has-error");
+                        self.hideUsernameExists = "false";
+                        angular.element(document.querySelector("#check-usernameexists")).removeClass("ng-hide");
+                        return;
+                    } else {
+                        angular.element(document.querySelector("#username-fg")).removeClass("has-error");
+                        self.hideUsernameExists = "false";
+                        angular.element(document.querySelector("#check-usernameexists")).addClass("ng-hide");
+                    }
+                });
+
 
 
             }
-            
+
             self.test = "kkkkkjjjjj";
         }]
     });
