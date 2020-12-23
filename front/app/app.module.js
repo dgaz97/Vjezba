4bdd94ac76f3932c9fb33b36a76348e539bdb691
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-12-23 12:28:20 +0100

    Dodan redirect, ako korisnik nije ulogiran

--- front/app/app.module.js
+++ front/app/app.module.js
@@ -16,3 +16,13 @@ angular
     'myApp.login-view',
     'myApp.version'
   ])
+  .run(['$window','$rootScope', '$location', function ($window, $rootScope, $location) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+      if ($window.sessionStorage.loggedIn!="true") {
+        if(next.originalPath!="/register"&&next.originalPath!="/login"){
+          event.preventDefault();
+          $location.path('/login');
+        }
+      }
+    })
+  }]);
