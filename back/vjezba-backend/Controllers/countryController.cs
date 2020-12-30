a9b58e7367285597dcda89817a6badc166d2d763
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-12-30 22:57:58 +0100

    Dodan controller za dohvat popisa država

---
+++ back/vjezba-backend/Controllers/countryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace vjezba_backend.Controllers
+{
+    [EnableCors(origins: "http://localhost:8000, https://localhost:8000", headers: "*", methods: "*")]
+    public class countryController : ApiController
+    {
+        // GET api/<controller>
+        public HttpResponseMessage Get()
+        {
+            VjezbaEntities db = new VjezbaEntities();
+            StringBuilder sb = new StringBuilder();
+            sb.Append($@"{{");
+            sb.Append($@"""success"":true,");
+            sb.Append($@"""countries"":[");
+
+            (from x in db.countryList select x).ToList().ForEach(x =>
+            {
+                sb.Append(x.ToJson());
+                sb.Append(",");
+            });
+            sb.Length--;
+            sb.Append($@"]}}");
+            return generateResponse(HttpStatusCode.OK, sb);
+        }
+
+        private HttpResponseMessage generateResponse(HttpStatusCode status, StringBuilder sb)
+        {
+            HttpResponseMessage m = new HttpResponseMessage(status);
+            m.Content = new StringContent(sb.ToString(), System.Text.Encoding.UTF8, "application/json");
+            return m;
+        }
+    }
+}
