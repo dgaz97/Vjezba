b5bcc0d46cc92de3d19e6b048becc2706906a526
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-12-31 16:41:58 +0100

    front: dohvaćanje imena države preko ISO koda

--- front/app/core/services/countries.service.js
+++ front/app/core/services/countries.service.js
@@ -6,4 +6,14 @@ angular.module('core.countries')
         },
         isArray:true
     })
+}])
+.factory('CountryName',['$resource', function($resource){
+    return $resource('https://localhost:44385/api/country/countryName?code=:code', {}, {
+        getCountryName:{
+            method:'GET'
+        },
+        params:{
+            code:"@code"
+        }
+    })
 }]);
