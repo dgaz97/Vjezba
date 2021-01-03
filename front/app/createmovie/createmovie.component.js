3dc6eb0e1894df4fb22b146107626ee0f8ada133
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-03 19:18:09 +0100

    front: Kod stvaranja filmova, sad se filmu mogu dodati žanrovi

--- front/app/createmovie/createmovie.component.js
+++ front/app/createmovie/createmovie.component.js
@@ -1,6 +1,7 @@
 angular.module('myApp.createMovie').component('createMovie', {
     templateUrl: "createmovie/createmovie.template.html",
-    controller: ['AddMovie', 'CountryList', '$window', function CreateMovieController(AddMovie, CountryList, $window) {
+    controller: ['AddMovie', 'CountryList', 'AllGenres', 'AddGenresToMovie', '$window'
+    , function CreateMovieController(AddMovie, CountryList, AllGenres, AddGenresToMovie, $window) {
         var self = this;
 
         self.title;
@@ -9,12 +10,14 @@ angular.module('myApp.createMovie').component('createMovie', {
         self.duration;
         self.status;
         self.releaseDate;
+        self.genres=[];
 
         self.maxDate = new Date();
 
         self.durationOK = false;
         self.titleOK = false;
         self.dateOK = false;
+        self.genreOK = false;
         self.allOK = false;
 
         self.durationRegex = /^([0-9]{1,}){1}:([0-5][0-9]){1}:([0-5][0-9]){1}$/;
@@ -22,6 +25,11 @@ angular.module('myApp.createMovie').component('createMovie', {
         CountryList.getCountries().$promise.then(function (data) {
             self.countries = data.countries;
         });
+
+        AllGenres.getAllGenres().$promise.then(function(data){
+            self.allGenres = data.genres;
+        });
+
 
         self.submit = function ($event) {
             if (!self.allOK) {
@@ -37,13 +45,18 @@ angular.module('myApp.createMovie').component('createMovie', {
                 status: self.status,
                 releaseDate: self.releaseDate.toDateString()
             }).$promise.then(function (data) {
-                if (data.success == true)
-                    $window.location.href = "#!/movieDetails/" + $routeParams.id;
+                if (data.success == true){
+                    AddGenresToMovie.addGenresToMovie({idMovie:data.id,listOfGenreIds:self.genres}).$promise.then(function(data2){
+                        if (data2.success==true){
+                            $window.location.href="#!/movieDetails/"+data.id;
+                        }
+                    })
+                }
             });
         }
 
         self.checkOK = function () {
-            self.allOK = self.durationOK && self.titleOK && self.dateOK;
+            self.allOK = self.durationOK && self.titleOK && self.dateOK && self.genreOK;
         }
 
         self.checkDuration = function () {
@@ -59,6 +72,10 @@ angular.module('myApp.createMovie').component('createMovie', {
 
         self.checkTitle = function () {
             self.titleOK = !(!self.title || /^\s*$/.test(self.title));
+            self.checkOK();
+        }
+        self.checkGenre = function () {
+            self.genreOK = !(self.genres === undefined || self.genres.length==0);
             self.checkOK();
         }
 
