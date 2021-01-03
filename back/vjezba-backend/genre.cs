edbb11fd47f98655c8b2eaeb0d87bb64bb60bb72
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-03 15:37:53 +0100

    ažuriran model, maknut opis žanrova

--- back/vjezba-backend/genre.cs
+++ back/vjezba-backend/genre.cs
@@ -22,7 +22,6 @@ namespace vjezba_backend
     
         public int Id { get; set; }
         public string name { get; set; }
-        public string description { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<filmEntryHasGenre> filmEntryHasGenre { get; set; }
