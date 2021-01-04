bba5c7836d902fb7224b302cfa873ac00960feee
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-01-04 15:06:57 +0100

    Na popisu svih filmova, dodan TreeMap koji pokazuje koliko filmova ima određeni žanr

--- front/app/allmovies/allmovies.component.js
+++ front/app/allmovies/allmovies.component.js
@@ -12,5 +12,46 @@ angular.module('myApp.allMovies').component('allMovies', {
             self.listOfMovies = data.filmEntries;
         });
 
+
+        MovieCountPerGenre.movieCountPerGenre().$promise.then(function (data) {
+            self.genreCount = data;
+            console.log(self.genreCount);
+            console.log(typeof (self.genreCount));
+
+            var d = new kendo.data.HierarchicalDataSource({
+                data: self.genreCount,
+                schema: {
+                    model: {
+                        children: "genres"
+                    }
+                }
+            })
+            console.log(d);
+
+
+            self.treemapOptions = {
+                dataSource: d,
+                valueField: "count",
+                textField: "name",
+                colors: [
+                    ["#0c81c5", "#c5dceb"], ["#3aa2de", "#d8ecf8"],
+                    ["#449000", "#dae9cc"], ["#76b800", "#dae7c3"],
+                    ["#ffae00", "#f5e5c3"], ["#ef4c00", "#f1b092"],
+                    ["#9e0a61", "#eccedf"]
+                ]
+            };
+
+            self.tooltipOptions = {
+                filter: "div",
+                content: function (e) {
+                    var treemap = angular.element("#treemap").data("kendoTreeMap");
+                    var item = treemap.dataItem(e.target.closest(".k-treemap-tile"));
+                    return item.count;
+                },
+                autoHide: true,
+                position: "auto"
+            }
+        })
+
     }]
 })
